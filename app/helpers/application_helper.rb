b0383e2b4da230d02ed820f8e4a72d07c6139c7a
kim hyunhak <kim.hyunhak@gmail.com>
2014-12-10 05:04:07 +0900

    이미지 변경

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,10 @@ module ApplicationHelper
   def present_link_text(student_attendance)  
     if student_attendance == nil || student_attendance.present == nil
       image_tag('n.png')
-      "빈칸"
     elsif student_attendance.present == true
       image_tag('o.png')
     elsif student_attendance.present == false
-      "결석"
+      image_tag('x.png')
     end
   end 
 end
